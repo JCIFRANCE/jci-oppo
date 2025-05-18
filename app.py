3b6ad6dfc5c37ef750c211fd4a22284e39daf057
JCIFRANCE <international@jcef.asso.fr>
2025-05-18 11:22:31 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pandas as pd
 import plotly.graph_objects as go
+import plotly.io as pio
+pio.templates.default = "plotly"
 
 df = pd.read_csv("https://docs.google.com/spreadsheets/d/147E7GhixKkqECtBB1OKGqSy_CXt6skrucgHhPeU0Dog/export?format=csv", encoding="utf-8")
 df["Forme"] = df["Forme"].str.strip().str.capitalize()
