8211338b14a738e6be9f027bdfd5d7f24e659d08
JCIFRANCE <international@jcef.asso.fr>
2025-05-20 15:51:58 +0200

    Update app.py

--- app.py
+++ app.py
@@ -183,16 +183,11 @@ df = load_data()
 
 st.markdown("""
 <h1>Les opportunités de la Jeune Chambre ... en Donuts 🍩</h1>
-
 <h2>Identifie facilement les opportunités de la Jeune Chambre qui te correspondent !</h2>
-
 **ETAPE 1. Personnalise tes préférences**  
 Utilise les curseurs et étiquettes à gauche pour faire ressortir les opportunités qui te ressemblent le plus.
-
 **ETAPE 2. Lis la cartographie en un coup d’œil**  
-Le cercle extérieur indique comment tu préfères t’impliquer. Le Cercle intérieur montre ce que tu souhaites développer à travers ton engagement. Les icônes dans le titre représentent la forme que prend l’opportunité (ex. formation, événement, projet…).  
-Le centre précise la portée de l’opportunité.
-
+Le cercle extérieur indique comment tu préfères t’impliquer. Le Cercle intérieur montre ce que tu souhaites développer à travers ton engagement. Les icônes dans le titre représentent la forme que prend l’opportunité (ex. formation, événement, projet…). Le centre précise la portée de l’opportunité.
 **Explore, ajuste, découvre ce qui te motive, et profite du plaisir de l'engagement !**
 """, unsafe_allow_html=True)
 
