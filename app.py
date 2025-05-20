6e8579abec2bfd7070ed2f891a2a60888256c5dc
JCIFRANCE <international@jcef.asso.fr>
2025-05-20 18:49:20 +0200

    Update app.py

--- app.py
+++ app.py
@@ -73,10 +73,10 @@ verbe_map = {
     "Rencontrer": "Se rencontrer"
 }
 verbe_icons = {
-    "Apprendre": ("<span style='background-color: #5E81D0; color: white; padding: 2px 6px; border-radius: 4px;'>Apprendre</span>", "#5E81D0"),
-    "Célébrer": ("<span style='background-color: #EEBD63; color: white; padding: 2px 6px; border-radius: 4px;'>Célébrer</span>", "#EEBD63"),
-    "Responsabiliser": ("<span style='background-color: #B74659; color: white; padding: 2px 6px; border-radius: 4px;'>Prendre des responsabilités</span>", "#B74659"),
-    "Rencontrer": ("<span style='background-color: #79C28F; color: white; padding: 2px 6px; border-radius: 4px;'>Se rencontrer</span>", "#79C28F")
+    "Apprendre": ("<span style='background-color: #026587; color: white; padding: 2px 6px; border-radius: 4px;'>Apprendre</span>", "#026587"),
+    "Célébrer": ("<span style='background-color: #f9C701; color: white; padding: 2px 6px; border-radius: 4px;'>Célébrer</span>", "#f9C701"),
+    "Responsabiliser": ("<span style='background-color: #D72D73; color: white; padding: 2px 6px; border-radius: 4px;'>Prendre des responsabilités</span>", "#D72D73"),
+    "Rencontrer": ("<span style='background-color: #5DB23C; color: white; padding: 2px 6px; border-radius: 4px;'>Se rencontrer</span>", "#5DB23C")
 }
 descriptions_verbes = {
     "Apprendre": "se former, monter en compétence et grandir",
